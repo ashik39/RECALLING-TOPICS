968fca1965897652cdb8d47235aa0e9e714c6a72
saravanan-zaigo <asanashik7@gmail.com>
2021-05-27 10:36:01 +0530

    [App] useEffect works commented in App.js

--- recall/src/App.js
+++ recall/src/App.js
@@ -1,22 +1,23 @@
-import { useState } from 'react/cjs/react.development';
+//import { useState } from 'react/cjs/react.development';
 import './App.css';
-import UseEffectMain from './UseEffect/UseEffectMain';
+// import UseEffectMain from './UseEffect/UseEffectMain';
 
 function App() {
+  //Below are useEffect example works
   // const [parentData, setParentData] = useState();
-  var parentData = 'PARENT DATA';
-  const [fromchild, setFromChild] = useState('');
-  const get = (data) => {
-    setFromChild(data);
-  };
-  return (
-    <div className="App">
-      <div className="Content">
-        <UseEffectMain parentData={parentData} toParent={get} />
-      </div>
-      <h1>Count From Child:{fromchild} </h1>
-    </div>
-  );
+  // var parentData = 'PARENT DATA';
+  // const [fromchild, setFromChild] = useState('');
+  // const get = (data) => {
+  //   setFromChild(data);
+  // };
+  // return (
+  //   <div className="App">
+  //     <div className="Content">
+  //       <UseEffectMain parentData={parentData} toParent={get} />
+  //     </div>
+  //     <h1>Count From Child:{fromchild} </h1>
+  //   </div>
+  // );
 }
 
 export default App;
